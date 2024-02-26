899a1a961b3f6fd6795eb7837e326e1ba483ada1
Geoffrey E Matesky <matesky@sbcglobal.net>
2024-02-25 21:13:55 -0500

    added outputs for website url

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -9,10 +9,11 @@ function run(){
   const distFolder = core.getInput('dist-folder', { required: true });
   
   // 2) upload files
-  const s3Uri = `s3://${bucket}`
+  const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-  core.notice('Hello from my custom js Actions');
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+  core.setOutput('website-url', websiteUrl);  //set ouput
 }
 
 run();
